6db9afa03be24d6bc0799315c32d960ff4bbb523
Paula Castellanos <pcastellanos90@gmail.com>
2020-04-19 18:05:20 -0500

    include star component nested

---
+++ Angular-GettingStarted/APM/src/app/shared/star.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnChanges, Input } from '@angular/core';
+
+@Component({
+    selector: 'pm-star',
+    templateUrl: './star.component.html',
+    styleUrls:['./star.component.css']
+})
+export class StarComponent implements OnChanges{
+    starWidth: number;
+    @Input() rating : number;
+
+    ngOnChanges(): void {
+        this.starWidth = this.rating * 75/ 5;
+    }
+    
+}
