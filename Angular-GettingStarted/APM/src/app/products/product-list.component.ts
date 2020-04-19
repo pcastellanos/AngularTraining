465d11fea183ad407eef36dc4e873ff1e74f9f23
Paula Castellanos <pcastellanos90@gmail.com>
2020-04-19 17:16:51 -0500

    include IProduct

--- Angular-GettingStarted/APM/src/app/products/product-list.component.ts
+++ Angular-GettingStarted/APM/src/app/products/product-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { IProduct } from './product'
 @Component({
     selector : 'pm-products',
     templateUrl: './product-list.component.html'
@@ -9,8 +9,8 @@ export class ProductListComponent{
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean =false;
-    listFilter: string ='cart';
-    products: any[] = [
+    listFilter: string ='';
+    products: IProduct[] = [
         {
           "productId": 1,
           "productName": "Leaf Rake",
