bac661506ba6635cabdd3e057fc4d0b1ca990ec1
Paula Castellanos <pcastellanos90@gmail.com>
2020-04-20 19:03:13 -0500

    include guard to validate product id

--- Angular-GettingStarted/APM/src/app/products/product-detail/product-detail.component.ts
+++ Angular-GettingStarted/APM/src/app/products/product-detail/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../product';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -7,11 +8,29 @@ import { IProduct } from '../product';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string  = 'Product Detail';
-  product: IProduct; 
+  product: IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router) {
 
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product =  {
+      productId: id,
+      productName: 'Garden Cart',
+      productCode: 'GDN-0023',
+      releaseDate: 'March 18, 2019',
+      description: '15 gallon capacity rolling garden cart',
+      price: 32.99,
+      starRating: 4.2,
+      imageUrl: 'assets/images/garden_cart.png'
+    };
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
