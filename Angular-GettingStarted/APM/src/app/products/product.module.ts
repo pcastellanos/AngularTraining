537083b4b9bc50eea5fe32145d93965abadfa8b6
Paula Castellanos <pcastellanos90@gmail.com>
2020-04-25 16:22:25 -0500

    refactor: create share.module

--- Angular-GettingStarted/APM/src/app/products/product.module.ts
+++ Angular-GettingStarted/APM/src/app/products/product.module.ts
@@ -1,29 +1,23 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list.component';
-import { ConvertToSpacesPipe } from '../shared/convert-to-spaces';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { StarComponent } from '../shared/star.component';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ProductDetailGuard } from './product-detail/product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [
     ProductListComponent,
-    ConvertToSpacesPipe,
-    ProductDetailComponent,
-    StarComponent
-  ],
+    ProductDetailComponent
+   ],
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       {path: 'products', component: ProductListComponent},
       {path: 'products/:id', component: ProductDetailComponent, canActivate: [ProductDetailGuard]}
     ]),
+    SharedModule
   ]
 })
 export class ProductModule { }
